a31d6fca2b49f78c0d10bdb3dcef970c29a13b23
William Rutkowski <budski82@users.noreply.github.com>
2024-06-27 22:22:25 -0400

    jjyyyy

--- README.md
+++
@@ -1,2 +0,0 @@
-# Two-Port-Exp-BS-app
- Express serves SSR on port 3000 via views/pug && Browser-sync handles static public resources on port 8000
---
+++ views/server.js
@@ -0,0 +1,47 @@
+
+
+
+// Required ES6 external modules
+
+import express from 'express'
+import path    from 'path'
+import cors    from 'cors'
+
+// App Variables
+
+const app = express()
+const port = process.env.PORT || '8000'
+
+
+// Serve static files from the "public" directory
+app.use(cors());
+app.use(express.static('public'));
+
+
+// App configuration
+
+app.set("view engine", "pug");
+
+// Routes Defined
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home"})
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve('views/index.pug'));
+});
+
+
+// Server Active
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
+
+
+
+
+
+
+console.log(123999954)
